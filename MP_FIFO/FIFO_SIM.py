02bd2f77e1dfdf8fbb7a36b9e1a8a854cb56764f
Buddhika Kelum <164610416+KeKelum@users.noreply.github.com>
2025-12-07 09:10:38 +0530

    Delete MP_FIFO directory

--- MP_FIFO/FIFO_SIM.py
+++
@@ -1,6 +0,0 @@
-def validate_inputs(ref_str, frames_count):
-    if len(ref_str) == 0 or len(ref_str) > 10:
-        raise ValueError("Reference string must contain 1 to 10 pages.")
-    if not (3 <= frames_count <= 5):
-        raise ValueError("Number of frames must be between 3 and 5.")
-    return True
